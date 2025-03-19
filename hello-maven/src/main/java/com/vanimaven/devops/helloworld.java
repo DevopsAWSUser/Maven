913e6c0cf4bc83a5a0c4a5b0f1211d2a013a3fb6
vanitha <ddkumar.ch@gmail.com>
2025-03-19 20:04:51 +0530

    initial commit

---
+++ hello-maven/src/main/java/com/vanimaven/devops/helloworld.java
@@ -0,0 +1,8 @@
+//utils.java
+package main.java.com.vanimaven.devops;
+class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+
+  }
+}
---
+++ hello-maven/src/main/java/com/vanimaven/devops/utils.java
@@ -0,0 +1,7 @@
+//utils.java
+package main.java.com.vanimaven.devops;
+public class Utils {
+  public String getName() {
+    return "siva Academy";
+  }
+}
